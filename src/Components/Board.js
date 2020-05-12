4357149022328b3645ae8d15c5d976928db79275
Yari PL <yaroslav.khudchenko@gmail.com>
2020-05-12 22:02:43 +0200

    added Board component

---
+++ src/Components/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Board.scss';
+
+
+const Board = () => {
+    return (
+        <div className="Board">
+
+        </div>
+    );
+}
+
+export default Board;
