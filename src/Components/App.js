a7c5932b59506706383bbf2837a7b6eb18bd2320
Yari PL <yaroslav.khudchenko@gmail.com>
2020-05-12 20:50:26 +0200

    add Header component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import '../App.scss';
+import '../css/App.scss';
+import Header from './Header';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Header.scss';
+
+
+const Header = () => {
+    return (
+        <div className="Header">
+            
+        </div>
+    );
+}
+
+export default Header;
