ef2071805d41cde97cf244486638b41a7bc6c05b
Yari PL <yaroslav.khudchenko@gmail.com>
2020-05-13 03:27:51 +0200

    save

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../css/App.scss';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Board from "./Board";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Sidebar />
+      <Board />
     </div>
   );
 }
