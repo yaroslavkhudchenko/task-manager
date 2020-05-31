f3af78ece42835c433ea6939e127592139dccafa
Yari PL <yaroslav.khudchenko@gmail.com>
2020-05-31 21:23:19 +0200

    add router for add project

---
+++ back/routes/addProject.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();// import Router from express
+
+let Addproject = require('../models/addproject.model'); // import mongoose model
+
+// route => handles imcoming http get requests /users
+router.route('/addproject').post((req, res) => {
+    Addproject.find() // get list of all the users from mongodb database 
+        .then(user => res.json(user)) // in json
+        .catch(err => res.status(400).json('Error: ' + err)); // if error return 400 with err message
+});
+
+// route => if url /users/add run this post(if post only)
+router.route('/add').post((req, res) => {
+
+
+    const name = 'prokec';// req.body.name; // from the input - the rest are the default values for the new empty project
+    const nbTask = 0;
+    const tasks = []; 
+    const archived = false; 
+
+
+    const newProject = new Exercise({
+        name,
+        nbTask,
+        tasks,
+        archived
+    }); // create the Exercise with the given values
+
+    newProject.save() // save to databse
+        .then(() => res.json('Exercise added!'))
+        .catch(err => res.status(400).json('Error: ' + err)); // if error return 400 with err message
+});
+
+module.exports = router; // export router
