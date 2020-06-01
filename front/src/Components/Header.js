02dc3f6cb5d3e10b9c31a85e8bb3664e9cec9a36
Yari PL <yaroslav.khudchenko@gmail.com>
2020-06-01 20:22:41 +0200

    add adding custom name to project on add

--- front/src/Components/Header.js
+++ front/src/Components/Header.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../css/Header.scss';
 import { fakeData } from './../fakeData/fakeData';
 import axios from 'axios';
 
 const Header = () => {
-    let nameNeeded = false;
+
+    const [nameNeeded, nameNeededSet] = useState(false)
+
     // add new project to the database
     const AddProject = () => {
 
+        let name = document.querySelector('.addNameModule input').value;
         axios.post('http://localhost:5000/projects/addproject',
             {
-                name: `project${Math.floor(Math.random() * Math.floor(100))}`,
+                name: name,
                 nbTask: 0,
                 tasks: [],
                 archived: false
             }
         )
+        nameNeededSet(false);
 
     }
     return (
         <div className="Header">
-            <div className='addBoard' onClick={() => nameNeeded = true}>Add</div>
+            <div className='addBoard' onClick={() => nameNeededSet(true)}>Add</div>
             {nameNeeded ?
                 <div className='addNameModule'>
                     <div className='addNameModuleTitle'>Provide the name</div>
