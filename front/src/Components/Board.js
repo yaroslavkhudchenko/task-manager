22231459fb50d008e1d1e34a21c50661e49a1581
YariPL <yari.lune@gmail.com>
2020-06-10 16:13:07 +0200

    function to reorder added

--- front/src/Components/Board.js
+++ front/src/Components/Board.js
@@ -34,17 +34,23 @@ const Board = () => {
 
     }
    
+  /* const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
 
+    return result;
+  };
     const onDragFinish = (e) => {
       const items = reorder(
         this.state.items,
         result.source.index,
         result.destination.index
-      );
-
+      ); */
+/* 
       this.setState({
         items
-      });
+      }); */
     }
 
     return (
