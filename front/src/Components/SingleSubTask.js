c741dcf76033e0f44608e1883cf3306b554f0c71
YariPL <yari.lune@gmail.com>
2020-06-16 19:18:43 +0200

    save

--- front/src/Components/SingleSubTask.js
+++ front/src/Components/SingleSubTask.js
@@ -4,14 +4,25 @@ const SingleSubTask = ({ title, descr, setOpenDescr}) => {
     
     return(
         <div id="singleSubTaskModal">
-            <div id="singleSubTaskModalTitle">{title}</div>
-            <div id="singleSubTaskModalBody">{descr}</div>
-            <div
-                id="singleSubTaskModalClose"
-                onClick={() => setOpenDescr(false)}
-            >
-                close
-                    </div>
+            <div id='singleSubTaskModalContent'>
+                <div id="singleSubTaskModalTitle">
+                    <textarea defaultValue={title} onBlur={
+                        console.log('blueer1')
+                    } />
+                </div>
+                <div id="singleSubTaskModalBody">
+                    <label>Description</label>
+                    <textarea defaultValue={descr} onBlur={
+                        console.log('blueer2')
+                    } />
+                </div>
+                <div
+                    id="singleSubTaskModalClose"
+                    onClick={() => setOpenDescr(false)}
+                >
+                    close
+                </div>
+            </div>
         </div>
     )
 }
